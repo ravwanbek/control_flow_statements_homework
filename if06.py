7724f529e05dac6ed0872002b39ea35c795129cf
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-31 22:25:36 +0900

    6

--- if06.py
+++ if06.py
@@ -13,6 +13,16 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
+    if a==0:
+        x=0
+        k=0
+    if b==0:
+        y=0
+        l=0
+    if c==0:
+        z=0
+        m=0
+        
     if a>0:
         x=1
     else:
@@ -45,8 +55,9 @@ def main(a,b,c):
     j=k+l+m
 
     if i>j:
-        return print('there are a lot of positive numbers')
+        print('there are a lot of positive numbers')
     else:
-        return print('there are a lot of negative numbers') 
-    
-main(-3,-1,-5)
+        print('there are a lot of negative numbers') 
+    return
+
+main(2,2,-2)
