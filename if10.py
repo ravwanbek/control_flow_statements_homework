2190e321a162260e923b145961c0adf8e2b75873
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-01 00:21:22 +0900

    10

--- if10.py
+++ if10.py
@@ -13,4 +13,19 @@ def main(temp):
     Returns:
         string: the message to print
     """
-    return
+    if temp<0:
+        t="Freezing"
+    if 0<temp<11:
+        t="Very Cold"
+    if 10<temp<21:
+        t="Cold"
+    if 20<temp<31:
+        t="Normal"
+    if 30<temp<41:
+        t="Hot"
+    if temp>40:
+        t="Very Hot"    
+    
+    return t
+t=main(40)
+print(t)
