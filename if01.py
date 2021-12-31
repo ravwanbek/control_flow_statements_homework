5455725f272b3ceacf2d3a42b6469c15614bdfa0
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2021-12-31 18:43:38 +0900

    1

--- if01.py
+++ if01.py
@@ -6,4 +6,12 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+    if a>0:
+        x=a+1
+    else:
+        x=a
+     
+
+    return x
+x=main(-7)
+print(x)
